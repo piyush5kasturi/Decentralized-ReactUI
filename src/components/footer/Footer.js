9524c501bbc67edebc637e5f096bdb1e0ec2ca16
piyush5kasturi <piyushkasturi5@gmail.com>
2022-03-26 17:47:46 +0530

    Added my project

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer' id="about">
+            <div className="container">
+                <div className="col">
+                    <h4>Products</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Decentralized</p>
+                </div>
+                <div className="col">
+                    <h4>Developers</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Decentralized</p>
+                </div>
+                <div className="col">
+                    <h4>Governance</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Decentralized</p>
+                </div>
+                <div className="col">
+                    <h4>Community</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Decentralized</p>
+                </div>
+                <div className="col">
+                    <h4>About</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Decentralized</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
